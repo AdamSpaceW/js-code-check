53939a4c7fe791b358cb39943c7696be86d29f28
Adam W <w.m.mada@gmail.com>
2014-08-01 23:30:57 +0100

    Update test-1.js

--- test-1.js
+++ test-1.js
@@ -1,4 +1,10 @@
 
 function button_click() {
- 
+ var txt = $("#msg")[0].value;
+ if(!txt) {
+  return;
+ }
+ var curdat = new Date();
+ $("#messages").html( '<div style="width: 300px; display: inline-block; color: #00F;">' + txt + '</div><span style="color: #00F;">' + curdat.getHours() + ':' + curdat.getMinutes() + '</span><br />' + $("#messages").html().replace("#00F", "#000") );
+ $("#msg")[0].value = "";
 }
